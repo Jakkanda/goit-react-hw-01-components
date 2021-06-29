237804d4d4c97aeabb248f856ffcb702d672469b
Jakkanda <51709348+Jakkanda@users.noreply.github.com>
2021-06-29 19:48:44 +0300

    4 task js-part done

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css';
+
+export function TransactionHistory({ items }) {
+  return (
+    <table classname={styles.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  amount: PropTypes.number,
+  currency:PropTypes.string
+}
